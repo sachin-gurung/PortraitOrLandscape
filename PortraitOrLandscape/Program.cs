64bb86e0d1369811a459e5770d8df7568149ac72
Sachin Gurung <saching061@hotmail.com>
2019-11-10 18:40:55 -0800

    First Commit

--- PortraitOrLandscape/Program.cs
+++ PortraitOrLandscape/Program.cs
@@ -10,6 +10,25 @@ namespace PortraitOrLandscape
     {
         static void Main(string[] args)
         {
+            int height;
+            int width;
+
+            Console.Write("Enter the image height: ");
+            height = int.Parse(Console.ReadLine());
+            Console.Write("Enter the imange width: ");
+            width = int.Parse(Console.ReadLine());
+            if (height > width)
+                Console.WriteLine("Portrait");
+            else
+                Console.WriteLine("Landscape");
+            Console.ReadLine();
         }
     }
 }
+/*
+Sample Result:
+Enter the image height: 45
+Enter the imange width: 22
+Portrait
+*/
+
